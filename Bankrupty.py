56238bcb4a6dd984a7dd46120c223ad1d2c05b86
MeghanaVinakota <114896714+MeghanaVinakota@users.noreply.github.com>
2024-01-31 00:41:22 +0530

    Add files via upload

---
+++ Bankrupty.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 21:48:04 2024
+
+@author: megha
+"""
+
+#Importing Libraries
+import pandas as pd
+import numpy as np
+from scipy import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#To ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+bankruptcy=pd.read_csv("bankruptcy-prevention.csv")
+bankruptcy
